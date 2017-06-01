5cd8daacb79839f8d9c842ea45fdd032ac48ffe0
Anshu Raghuvanshi <anshuraghuvanshi@Anshus-MacBook-Air.local>
2017-06-01 08:48:29 +0530

    Updated the compute grade function

--- grade.py
+++ grade.py
@@ -1,17 +1,20 @@
-try:
- score=float(input("Enter the score from 0.0-1.0\n"))
- if score<0.1 or score>1.0:
-   print("Bad Score")
-except:
- print("Bad Score")
- exit(0)
-if 0.1>=score>=0.9:
- print("A")
-elif 0.8<=score<0.9:
- print("B")
-elif 0.7<=score<0.8:
- print("C")
-elif 0.6<=score<0.7:
- print("D")
-elif score<0.6:
- print("No Grade")
+def computegrade(grade): 
+  try:
+    score=float(grade)
+    if score<0.1 or score>1.0:
+      print("Bad Score")
+  except:
+    print("Bad Score")
+    exit(0)
+  if 1.0>=score>=0.9:
+    print("A")
+  elif 0.8<=score<0.9:
+    print("B")
+  elif 0.7<=score<0.8:
+    print("C")
+  elif 0.6<=score<0.7:
+    print("D")
+  elif score<0.6:
+    print("No Grade")
+
+computegrade(1.1)
